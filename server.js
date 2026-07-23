c54b01fff286987ad6d7be2555d49d0e5df37746
ABIR <abir@example.com>
2026-07-23 18:17:03 +0600

    Update default local server port to 3001

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const DB_PATH = path.join(__dirname, 'public', 'db.json');
 
 app.use(express.json());
